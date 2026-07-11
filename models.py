20cde6eb580c1abbcec22e078a032399060a4096
msistla96 <sistla.meena96@gmail.com>
2026-07-11 00:41:44 -0400

    fix: modify film id in WatchlistEntry to match refactored id in Film

--- models.py
+++ models.py
@@ -75,9 +75,10 @@ class CollectionEntry(db.Model):
 
 class WatchlistEntry(db.Model):
     """Represents a film a user wants to watch (saved for later)."""
+    # Refactor for Film changed the ID Datatype, modifying it here to match Film
     id = db.Column(db.String(36), primary_key=True, default=generate_uuid)
     user_id = db.Column(db.String(36), db.ForeignKey("user.id"), nullable=False)
-    film_id = db.Column(db.Integer, db.ForeignKey("film.id"), nullable=False)
+    film_id = db.Column(db.String(36), db.ForeignKey("film.id"), nullable=False)
     date_added = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
     public = db.Column(db.Boolean, default=True)
 
